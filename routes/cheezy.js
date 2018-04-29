6184d88d863ef9b9397d4adfd6c984487d97e9c0
Aprajita <aprajita212@gmail.com>
2018-04-28 23:41:35 -0700

    Fixing routes for cheezy

--- routes/cheezy.js
+++ routes/cheezy.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/cheezy', function(req, res, next) {
-  res.render('cheezy_index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('cheezy', { title: 'Express' });
 });
 
 
